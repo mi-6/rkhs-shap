a5d4e87d3158ff7c3f2a21a5bd60a473e2ab507c
Tobias Fausten <tobias.fausten@gmail.com>
2025-11-13 13:41:31 +0900

    argument type hints and formatting for exact shap

--- src/rkhs_shap_exact.py
+++ src/rkhs_shap_exact.py
@@ -6,12 +6,16 @@ import torch
 from scipy.special import binom
 import numpy as np
 from gpytorch.kernels import RBFKernel
+
 # from gpytorch.lazy import lazify
 from sklearn.linear_model import Ridge
 from numpy import sum
 from tqdm import tqdm
-from src.sampling import large_scale_sample_alternative, generate_full_Z, subsetting_full_Z
-
+from src.sampling import (
+    large_scale_sample_alternative,
+    generate_full_Z,
+    subsetting_full_Z,
+)
 
 
 class RKHSSHAP(object):
@@ -19,9 +23,16 @@ class RKHSSHAP(object):
     Implement the exact RKHS SHAP algorithm with no kernel approximation
     """
 
-    def __init__(self, X, y, lambda_krr = 1e-2, lambda_cme = 1e-4, lengthscale = None,):
+    def __init__(
+        self,
+        X: torch.Tensor,
+        y: torch.Tensor,
+        lambda_krr: float = 1e-2,
+        lambda_cme: float = 1e-4,
+        lengthscale: torch.Tensor | None = None,
+    ) -> None:
         """[summary]
-        
+
         Args:
             X ([torch tensor]): [data]
             y ([torch tensor]): [label]
@@ -33,7 +44,7 @@ class RKHSSHAP(object):
         # Store data
         self.n, self.m = X.shape
         self.X, self.y = X, y
-        self.X_scaled = (X/lengthscale).float()
+        self.X_scaled = (X / lengthscale).float() if lengthscale is not None else X.float()
         self.lengthscale = lengthscale
 
         lambda_krr = torch.tensor(lambda_krr, dtype=torch.float32)
@@ -52,15 +63,15 @@ class RKHSSHAP(object):
         alphas = Kxx.add_diag(lambda_krr).inv_matmul(self.y)
         self.alphas = alphas.float().reshape(-1, 1)
 
-        self.ypred = Kxx@alphas
-        self.rmse = torch.sqrt(torch.mean(self.ypred - self.y)**2)
+        self.ypred = Kxx @ alphas
+        self.rmse = torch.sqrt(torch.mean(self.ypred - self.y) ** 2)
 
     def _value_intervention(self, z, X_new):
 
-        X_new_scaled = X_new/self.lengthscale
+        X_new_scaled = X_new / self.lengthscale
 
         n_ = X_new.shape[0]
-        zc = (z == False)
+        zc = z == False
 
         reference = (self.ypred.mean() * torch.ones((1, n_))).float()
         self.reference = reference
@@ -69,26 +80,28 @@ class RKHSSHAP(object):
             # features all active
             new_ypred = self.alphas.T @ self.k(self.X_scaled, X_new_scaled).evaluate()
             return new_ypred - reference
-        
+
         elif z.sum() == 0:
             return 0
-        
+
         else:
             X_S, X_Sc = self.X_scaled[:, z], self.X_scaled[:, zc]
             Xp_S = X_new_scaled[:, z]
             K_SSp = self.k(X_S, Xp_S).evaluate().float()
             K_Sc = self.k(X_Sc, X_Sc)
 
-            KME_mat = K_Sc.evaluate().mean(axis=1)[:, np.newaxis] * torch.ones((self.n, n_))
+            KME_mat = K_Sc.evaluate().mean(axis=1)[:, np.newaxis] * torch.ones(
+                (self.n, n_)
+            )
 
             return self.alphas.T @ (K_SSp * KME_mat) - reference
 
     def _value_observation(self, z, X_new):
 
-        X_new_scaled = X_new/self.lengthscale
+        X_new_scaled = X_new / self.lengthscale
 
         n_ = X_new.shape[0]
-        zc = (z == False)
+        zc = z == False
 
         reference = (self.ypred.mean() * torch.ones((1, n_))).float()
         self.reference = reference
@@ -97,7 +110,7 @@ class RKHSSHAP(object):
             new_ypred = self.alphas.T @ self.k(self.X_scaled, X_new_scaled).evaluate()
 
             return new_ypred - reference
-        
+
         elif z.sum() == 0:
             return 0
 
@@ -108,23 +121,23 @@ class RKHSSHAP(object):
             K_Sc = self.k(X_Sc, X_Sc)
             K_SS = self.k(X_S, X_S)
 
-            Xi_S = (K_SS.add_diag(self.n * self.lambda_cme).inv_matmul(K_Sc.evaluate())).T
+            Xi_S = (
+                K_SS.add_diag(self.n * self.lambda_cme).inv_matmul(K_Sc.evaluate())
+            ).T
 
             return self.alphas.T @ (K_SSp * (Xi_S @ K_SSp)) - reference
-
 
     def fit(self, X_new, method, sample_method, num_samples=100, wls_reg=1e-10):
 
         n_ = X_new.shape[0]
 
-        if sample_method=="MC":
+        if sample_method == "MC":
             Z = large_scale_sample_alternative(self.m, num_samples)
-        elif sample_method=="MC2":
+        elif sample_method == "MC2":
             Z = generate_full_Z(self.m)
             Z = subsetting_full_Z(Z, samples=num_samples)
         else:
             Z = generate_full_Z(self.m)
-
 
         # Set up containers
         epoch = Z.shape[0]
@@ -135,12 +148,14 @@ class RKHSSHAP(object):
 
         for row in tqdm(Z):
             if np.sum(row) == 0 or np.sum(row) == self.m:
-                weights.append(1e+5)
-                
+                weights.append(1e5)
+
             else:
                 z = row
-                weights.append((self.m - 1) / (binom(self.m, sum(z)) * sum(z) * (self.m - sum(z))))
-            
+                weights.append(
+                    (self.m - 1) / (binom(self.m, sum(z)) * sum(z) * (self.m - sum(z)))
+                )
+
             if method == "O":
                 Y_target[count, :] = self._value_observation(row, X_new)
             elif method == "I":
@@ -148,16 +163,14 @@ class RKHSSHAP(object):
             else:
                 raise ValueError("Must be either interventional or observational")
 
-            count += 1        
+            count += 1
 
         clf = Ridge(wls_reg)
         clf.fit(Z, Y_target, sample_weight=weights)
 
-        self.full_shapley_values_ = np.concatenate([clf.intercept_.reshape(-1, 1), clf.coef_], axis=1)
+        self.full_shapley_values_ = np.concatenate(
+            [clf.intercept_.reshape(-1, 1), clf.coef_], axis=1
+        )
         self.SHAP_LM = clf
 
         return clf.coef_
-
-
-
-
