c61b92089bcbcb16ea863a7f551f42ea21444172
Tobias Fausten <tobias.fausten@gmail.com>
2025-12-11 17:07:23 +0900

    remove kernel_ridge_regression.py

--- src/rkhs_shap/__init__.py
+++ src/rkhs_shap/__init__.py
@@ -1,15 +1,11 @@
 """RKHS-SHAP: Shapley Values for Kernel Methods"""
 
 from rkhs_shap.kernel_approx import Nystroem
-from rkhs_shap.kernel_ridge_regression import KernelRidgeRegressor
 from rkhs_shap.rkhs_shap_approx import RKHSSHAPApprox
 from rkhs_shap.rkhs_shap_exact import RKHSSHAP
-from rkhs_shap.shapley_regulariser import ShapleyRegulariser
 
 __all__ = [
     "RKHSSHAP",
     "RKHSSHAPApprox",
-    "KernelRidgeRegressor",
     "Nystroem",
-    "ShapleyRegulariser",
 ]
